1e1576d10ee879a0f1e3fb4ed940b9684cc305a7
RubalSingh24 <ms9056316876@gmail.com>
2025-06-09 12:30:12 +0530

    First

---
+++ script.js
@@ -0,0 +1,8 @@
+var icon = document.querySelector("#home_icon img");
+icon.addEventListener("mouseenter", function () {
+  icon.style.width = "20px";
+});
+icon.addEventListener("mouseleave", function () {
+  icon.style.width = "25px";
+});
+
